a11d3277cf01fe439362eea1f0fe3f608077ca11
Itsuki06 <trinhtung0604@gmail.com>
2025-10-08 20:12:30 +0700

    1 - Init project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connect.js';
+import authRouter from './routes/auth.route.js'; 
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT ||5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use('/auth', authRouter);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
