33e8122144803f93569561743dd3ce0086245ef2
Itsuki06 <trinhtung0604@gmail.com>
2025-10-08 20:28:44 +0700

    4 - Create auth-controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+export const signup = (req, res) => {
+    // Handle signup logic here
+    res.send('Signup route');
+};
+
+export const login = (req, res) => {
+    // Handle signup logic here
+    res.send('Signup route');
+};
+
+export const logout = (req, res) => {
+    // Handle signup logic here
+    res.send('Signup route');
+};
