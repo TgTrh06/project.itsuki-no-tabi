cb885f83ceae7c8fc5821b42ffec5a2875a0fadf
Itsuki06 <trinhtung0604@gmail.com>
2025-10-09 23:55:26 +0700

    6 - Update auth-route-signup

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import {
+    signup
+} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -7,10 +10,7 @@ router.get('/login', (req, res) => {
     res.send('Login route');
 });
 
-router.post('/signup', (req, res) => { 
-    // Handle signup logic here
-    res.send('Signup route');
-}); 
+router.post('/signup', signup); 
 
 router.post('/logout', (req, res) => { 
     // Handle signup logic here
