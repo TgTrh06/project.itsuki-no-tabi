46829f8563faac69dc114f550c93b50804bda0fd
Itsuki06 <trinhtung0604@gmail.com>
2025-10-10 06:32:56 +0700

    11 - Create gen token utils

---
+++ backend/utils/generateTokenAndSetCookie.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+
+export const generateTokenAndSetCookie = (res, userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '7d', // Token valid for 7 days
+    });
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // Set to true in production
+        sameSite: 'strict',
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    return token;
+}
