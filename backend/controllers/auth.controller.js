ad8a38bddb2862a7929296f3fb37f3bd4d7210d3
Itsuki06 <trinhtung0604@gmail.com>
2025-10-11 08:52:59 +0700

    14 - Update auth-controller: add verifiy email

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -12,6 +12,8 @@ export const signup = async (req, res) => {
         }
 
         const userAlreadyExists = await User.findOne({ email });
+        console.log("userAlreadyExists", userAlreadyExists);
+
         if (userAlreadyExists) {
             return res.status(400).json({ message: 'User already exists' });
         }
@@ -42,6 +44,36 @@ export const signup = async (req, res) => {
         })
     } catch (error) {
         res.status(400).json({ success: false, message: error.message });
+    }
+};
+
+export const verifyEmail = async (req, res) => {
+    const { code } = req.body;
+    try {
+        const user = await User.findOne({
+            verificationToken: code,
+            verificationTokenExpiresAt: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired verification code"});
+        }
+
+        user.isVerified = true;
+        user.verificationToken = undefined;
+        user.verificationTokenExpiresAt = undefined;
+        await user.save();
+
+        res.status(200).json({ 
+            success: true, 
+            message: "Email verified successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        })
+    } catch (error) {
+
     }
 };
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,32 +1,35 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
+const userSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        lastLogin: {
+            type: Date,
+            default: null
+        },
+        isVerified: {
+            type: Boolean,
+            default: false
+        },
+        resetPasswordToken: String,
+        resetPasswordExpiresAt: Date,
+        verificationToken: String,
+        verificationTokenExpiresAt: Date,
     },
-    password: {
-        type: String,
-        required: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    lastLogin: {
-        type: Date,
-        default: null
-    },
-    isVerified: {
-        type: Boolean,
-        default: false
-    },
-    resetPasswordToken: String,
-    resetPasswordExpiresAt: Date,
-    verificationToken: String,
-    verificationTokenExpiresAt: Date,
-},{timestamps:true});
+    {timestamps:true}
+);
 
 export const User = mongoose.model('User', userSchema);
 
