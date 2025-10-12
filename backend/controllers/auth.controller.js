ceaec64817dde82376d3a8b26ff4e3b39ef92b2e
Itsuki06 <trinhtung0604@gmail.com>
2025-10-12 21:44:46 +0700

    20 - Update auth-controller: add resetPassword controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -148,8 +148,35 @@ export const forgotPassword = async (req, res) => {
     }
 }
 
-export const resetPassword = (req, res) => {
+export const resetPassword = async (req, res) => {
+    try {
+        const { token } = req.params;
+        const { password } = req.body;
 
+        const user = await User.findOne({
+            resetPasswordToken: token,
+            resetPasswordExpiresAt: { $gt: Date.now() },
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false, message: "Invalid or expired reset token" });
+        }
+
+        // Update password
+        const hashedPassword = await bcryptjs.hash(password, 10);
+
+        user.password = hashedPassword;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpiresAt = undefined;
+        await user.save();
+
+        await sendVerificationEmail(user.email);
+
+        res.status(200).json({ success: true, message: "Password reset successfully " });
+    } catch (error) {
+        console.log("Error in resetPassword ", error);
+        res.status(400).json({ success: false, message: error.message });
+    }
 }
 
 export const checkAuth = (req, res) => {}
