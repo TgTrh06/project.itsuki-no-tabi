8c38dc1c5fd849351c16f7a7862ac92252c31131
Itsuki06 <trinhtung0604@gmail.com>
2025-10-12 21:53:06 +0700

    21 - Update mailtrap: add sendPasswordResetEmail

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -170,7 +170,7 @@ export const resetPassword = async (req, res) => {
         user.resetPasswordExpiresAt = undefined;
         await user.save();
 
-        await sendVerificationEmail(user.email);
+        await sendResetSuccessEmail(user.email);
 
         res.status(200).json({ success: true, message: "Password reset successfully " });
     } catch (error) {
--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -26,4 +26,22 @@ export const sendVerificationEmail = async (email, verificationToken) => {
 
 // export const sendWelcomeEmail
 
-// export const sendPasswordResetEmail
+export const sendPasswordResetEmail = async (email, resetURL) => {
+    const recipient = [{ email }];
+
+    try {
+        const response = await mailtrapClient.send({
+            from: sender,
+            to: recipient,
+            subject: "Reset your password",
+            html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+            category: "Password Reset",
+        });
+    } catch (error) {
+        console.error("Error sending password reset email ", error);
+
+        throw new Error(`Error sending password reset email: ${error}`);
+    }
+}
+
+// export const sendResetSuccessEmail
