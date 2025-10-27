ae11cacf4a390d1248e3d5da8132dde43eeb1b0d
Itsuki06 <trinhtung0604@gmail.com>
2025-10-27 21:45:40 +0700

    54 - Update blog route

--- backend/src/routes/blog.route.js
+++ backend/src/routes/blog.route.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import { verifyToken } from '../middleware/verifyToken.js';
 import { 
     getAllPosts, 
-    getPost 
+    getPost,
+    createPost,
+    updatePost,
+    deletePost,
+    toggleLikePost
 } from '../controllers/blog.controller.js';
 
 const router = express.Router();
 
+// Public routes
 router.get("/", getAllPosts);
 router.get("/:id", getPost);
+
+// Protected routes - require authentication
+router.use(verifyToken);
+router.post("/", createPost);
+router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
+router.post("/:id/like", toggleLikePost);
 
 export default router;
