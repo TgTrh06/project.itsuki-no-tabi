048ea6b0a644e6f1d2fbca4479dcf4648399d0b5
Itsuki06 <trinhtung0604@gmail.com>
2025-11-10 23:07:15 +0700

    65 - Update routes

---
+++ frontend/src/routes/AuthRoutes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from '../pages/auth/LoginPage'
+import SignUpPage from '../pages/auth/SignUpPage'
+import ForgotPasswordPage from '../pages/auth/ForgotPasswordPage'
+import EmailVerificationPage from '../pages/auth/EmailVerificationPage'
+import ResetPasswordPage from '../pages/auth/ResetPasswordPage'
+
+const AuthRoutes = () => {
+    return (
+        <Routes>
+			<Route path="/login" element={<LoginPage />} />
+			<Route path="/signup" element={<SignUpPage />} />
+			<Route path="/forgot-password" element={<ForgotPasswordPage />} />
+			<Route path="/verify-email" element={<EmailVerificationPage />} />
+			<Route path="/reset-password/:token" element={<ResetPasswordPage />} />
+        </Routes>
+    );
+}
+
+export default AuthRoutes;
