d267cd53c988a94178fcf02daa66fd780f2d7521
Itsuki06 <trinhtung0604@gmail.com>
2025-11-12 22:53:34 +0700

    67 - Create destination model

---
+++ backend/src/models/destination.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const destinationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    svgId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String
+    },
+    imageUrl: {
+      type: String,
+    },
+    region: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Destination = mongoose.model("Destination", destinationSchema);
